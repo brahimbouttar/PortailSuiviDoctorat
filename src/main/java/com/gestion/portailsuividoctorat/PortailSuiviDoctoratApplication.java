549f2cd8026950bca9697982323914401aa778eb
brahimbouttar <brahimbouttar@gmail.com>
2026-03-03 13:47:13 +0000

    init

---
+++ src/main/java/com/gestion/portailsuividoctorat/PortailSuiviDoctoratApplication.java
@@ -0,0 +1,13 @@
+package com.gestion.portailsuividoctorat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortailSuiviDoctoratApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PortailSuiviDoctoratApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gestion/portailsuividoctorat/PortailSuiviDoctoratApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gestion.portailsuividoctorat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortailSuiviDoctoratApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
