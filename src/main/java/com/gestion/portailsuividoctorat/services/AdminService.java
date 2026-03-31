a66a4b329414faa61bc8dbebce89536f5152bbae
AymenEdd <ayman.eddahani@gmail.com>
2026-03-31 15:03:09 +0100

    change

--- src/main/java/com/gestion/portailsuividoctorat/services/AdminService.java
+++ src/main/java/com/gestion/portailsuividoctorat/services/AdminService.java
@@ -1,4 +1,16 @@
 package com.gestion.portailsuividoctorat.services;
 
+import com.gestion.portailsuividoctorat.entites.Doctorant;
+import com.gestion.portailsuividoctorat.entites.Utilisateur;
+
+import java.util.List;
+
 public interface AdminService {
+    List<Utilisateur> getAllUsers();
+    void assignRole(long userId, String role);
+    List<Doctorant> getAllDoctorants();
+    Doctorant getDoctorant(long id);
+    void deleteDoctorant(long id);
+    void assignSupervisor(long doctorantId, long encadrantId);
+
 }
--- src/main/java/com/gestion/portailsuividoctorat/services/DemandeService.java
+++ src/main/java/com/gestion/portailsuividoctorat/services/DemandeService.java
@@ -1,4 +1,13 @@
 package com.gestion.portailsuividoctorat.services;
 
+import com.gestion.portailsuividoctorat.entites.Demande;
+
+import java.util.List;
+
 public interface DemandeService {
+    Demande createDemande(Demande demande);
+    Demande updateDemande(Long id, Demande demande);
+    void deleteDemande(Long id);
+    Demande getDemandeById(Long id);
+    List<Demande> getAllDemandes();
 }
