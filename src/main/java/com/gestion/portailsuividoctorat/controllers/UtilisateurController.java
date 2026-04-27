f09b916f32263b675ce57fb7eb6888050277b81f
brahimbouttar <brahimbouttar@gmail.com>
2026-04-27 11:23:57 +0100

    add navbar and view of utilisateur

--- src/main/java/com/gestion/portailsuividoctorat/controllers/UtilisateurController.java
+++ src/main/java/com/gestion/portailsuividoctorat/controllers/UtilisateurController.java
@@ -4,11 +4,11 @@ import com.gestion.portailsuividoctorat.entites.Utilisateur;
 import com.gestion.portailsuividoctorat.services.UtilisateurServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
-@RequestMapping("/api/Utilisateur")
-@RestController
+@RequestMapping("/Utilisateur")
+//@RestController
 @Controller
 public class UtilisateurController {
     @Autowired
@@ -18,12 +18,14 @@ public class UtilisateurController {
         return utilisateurService.createUtilisateur(u);
     }
     @GetMapping("/details/{id}")
-    public Utilisateur trouverUtilisateur(@PathVariable Long id){
-        return utilisateurService.findUtilisateur(id);
+    public String details(Model model, @PathVariable Long id) {
+        model.addAttribute("Utilisateur",utilisateurService.findUtilisateur(id));
+        return "Utilisateur/Details";
     }
     @GetMapping("/all")
-    public List<Utilisateur> findAllUtilisateurs() {
-        return utilisateurService.findAllUtilisateurs();
+    public String list(Model model) {
+        model.addAttribute("Utilisateur", utilisateurService.findAllUtilisateurs());
+        return "Utilisateur/Liste";
     }
     @DeleteMapping("/delete/{id}")
     public void deleteUtilisateur(@PathVariable Long id){
